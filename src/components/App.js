575e7ec8dd7119f74f51a9bca1a39e8b7415a626
Earl Perez <cayetanoperez@earl-mbp.attlocal.net>
2018-07-18 02:23:39 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from React Webpack</h1>
+        {/* <p>Testing hot reload</p> */}
+      </div>
+    );
+  }
+}
+
+export default App;
