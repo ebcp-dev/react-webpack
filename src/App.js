10a8bc28826c9231a16f8d922dde16933efbd52b
Earl Perez <ebcp.dev@gmail.com>
2018-08-01 02:51:16 -0700

    added component folder with example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import UI from './components/UI';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello from React Webpack</h1>
-        {/* <p>Testing hot reload</p> */}
+        <UI />
       </div>
     );
   }
